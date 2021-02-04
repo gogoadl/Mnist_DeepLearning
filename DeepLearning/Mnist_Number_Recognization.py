02f6d72f9baf1f9e7a96638253c3a9b391447284
DESKTOP-5TJ7OBF\HyeonWoo <gogoadl@naver.com>
2021-02-04 22:51:22 +0900

    [refactor] 코드 정리

--- DeepLearning/Mnist_Number_Recognization.py
+++ DeepLearning/Mnist_Number_Recognization.py
@@ -1,27 +1,13 @@
 import matplotlib.pyplot as plt  # 그림으로 보기 위한 matplotlib 라이브러리 import
 from tensorflow.keras.datasets import mnist  # 라이브러리가 기본으로 제공하는 mnist 데이터셋
 import numpy as np
-import tensorflow as tf
-from keras import backend as K
 from keras.datasets import mnist
-from keras.layers import Flatten
-
-# 과정 생각해보기
-# 입력데이터 x(28 x 28), y(10) 를 불러온다.
-# 입력데이터 x에 대한 가중치 배열을 x와 동일한 크기로 생성, 편향 b도 생성해준다.
-# y의 값의 범위가 0 ~ 9 이기 때문에 소프트맥스 함수를 통해 값 얻어내기
-# y의 값을 one hot encoding 하여 얻어내고, 결과값과 실제값을 비교
-
-# 우선 x1w1 * x2w2 * x3w3 ... + b 가 되어야 겠지?
-
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
 
 # fan_in : input neuron
 # fan_out : output neuron
 
-def xavier_init(fan_in, fan_out, w):
-    w = np.random.randn(fan_in, fan_out) / np.sqrt(fan_in)
+def xavier_init(fan_in, fan_out):
+    w = np.random.randn(fan_in, fan_out) / np.sqrt(fan_in) # Xavier 초기화 방법은 표준 정규 분포를 입력 개수의 표준 편차로 나누는 방법이다.
     return w
 
 def sigmoid(x):
@@ -33,6 +19,7 @@ def softmax(x_data):
     x_data = np.exp(x_data)
     return x_data / np.sum(x_data)
 
+(X_train, y_train), (X_test, y_test) = mnist.load_data() # mnist에서 데이터셋을 가져온다.
 
 X_train, X_test = X_train / 255.0 , X_test / 255.0 # 0 ~ 255 의 값을 0 ~ 1 사이로 변환
 
@@ -40,41 +27,48 @@ y_train_hot = np.eye(10)[y_train] # y_train 의 값을 10개의 리스트에서 
 
 accurancy = 0
 
+epochs = 2000
+
 if __name__ == '__main__':
-    for i in range(1000):
-        w1_size = (2, 784)
-        x1 = np.array(X_train[i]).flatten()
-        w1 = np.zeros(784)
-        w1 = xavier_init(784, 100, w1)
-        b1 = np.zeros(100)
-        # w1 = xavier_init(784, 2, w1)
-        # w2 = np.zeros(shape=784)  # 가중치
-        b2 = np.zeros(10)
-        n1 = sigmoid(np.dot(x1, w1) + b1)
-        # print(y_train_hot)
+    max_arr = []
+    min_arr = []
+    for i in range(epochs):
 
-        w2 = np.zeros(100)
-        w2 = xavier_init(100, 10, w2)
-        n2 = np.dot(n1, w2) + b2
+        input_x = np.array(X_train[i]).flatten() # 28 X 28 배열을 일차원 배열으로 바꿔주기
+        input_w = xavier_init(784, 2) # xavier 가중치 초기화
+        input_b = np.zeros(2) # bias 초기화
 
-        result = softmax(n2)
+        hidden_w1 = xavier_init(2, 3)
+        hidden_b1 = np.zeros(3)
 
+        print("------")
+        # print("hidden_w1", hidden_w1)
+        # print("hidden_w1 최댓값", np.max(hidden_w1))
+        # print("hidden_w1 최솟값", np.min(hidden_w1))
+        max_arr.append(np.max(hidden_w1))
+        min_arr.append(np.min(hidden_w1))
+        print("------")
+        hidden_w2 = xavier_init(3, 10)
+        hidden_b2 = np.zeros(10)
+
+        hidden_n1 = sigmoid(np.dot(input_x, input_w) + input_b)
+
+        hidden_n2 = sigmoid(np.dot(hidden_n1, hidden_w1) + hidden_b1)
+        # print(hidden_n2)
+        output_n = np.dot(hidden_n2, hidden_w2) + hidden_b2
+
+        result = softmax(output_n)
+        # print(result)
         predict = np.argmax(result) == np.argmax(y_train_hot[i])
         # print(np.argmax(result))
         # print(predict)
-        if predict == True:
+        if predict:
             accurancy += 1.0
 
-        if i % 100 == 0:
-            print("accurancy : ", accurancy)
+    accurancy = accurancy / epochs
 
-
-
-        # for x_data, y_data in data:
-        #     a_diff = x_data * (sigmoid(a * x_data + b) - y_data)
-        #     b_diff = sigmoid(a * x_data + b) - y_data
-        #     a = a - lr * a_diff
-        #     b = b - lr * b_diff
-
+    print("accuracy : %.4f" % (accurancy))
+    print("max_arr 최댓값", np.max(max_arr))
+    print("min_arr 최솟값", np.min(min_arr))
     # plt.imshow(X_train[0])
     # plt.show()
